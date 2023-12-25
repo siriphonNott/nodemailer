72c3862037d0e09c00e2264dd0bf13939fccd564
nottdark <nottdark.dev@gmail.com>
2023-12-26 01:29:29 +0700

    Fix variable port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors')
 const app = express();
-const port = 3000 || process.env.PORT;
+const port =  process.env.PORT || 3000
 const ejs = require("ejs");
 
 // Load environment variables
